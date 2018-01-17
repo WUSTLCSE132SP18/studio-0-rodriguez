04a0aa30ae9249552e33f090742a6cd0f8a422d0
Nathaniel Rodriguez <nate-rod@live.com>
2018-01-17 15:12:31 -0600

    Add files via upload
    testing upload for studio 0

---
+++ heartbeat.ino
@@ -0,0 +1,18 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println("Beginning timing:");
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  long time = millis(); //begin count of ms
+  Serial.print(time / 1000); //conversion to seconds
+  Serial.println(" seconds have passed.");
+  digitalWrite(13, HIGH);
+  delay(500); //pause
+  digitalWrite(13, LOW);
+  delay(500); //pause again
+  
+}
